db1c1491a815c206d27545720395868b23567cf8
Daniel Silvestre <djlebersilvestre@gmail.com>
2015-03-17 17:58:14 -0300

    Better naming convention

--- commands.sh
+++ commands.sh
@@ -6,29 +6,29 @@ PG_PASS=testing
 PG_HOST=127.0.0.1
 PG_PORT=5432
 
-pg-setup() {
+pg_setup() {
   docker run --name $DKR_PG_CONTAINER -e POSTGRES_PASSWORD=$PG_PASS -d -p $PG_HOST:$PG_PORT:$PG_PORT $DKR_IMAGE
 }
 
-pg-start() {
-  pg-stop
+pg_start() {
+  pg_stop
   docker start $DKR_PG_CONTAINER
 }
 
-pg-restart() {
-  pg-start
+pg_restart() {
+  pg_start
 }
 
-pg-stop() {
+pg_stop() {
   docker stop $DKR_PG_CONTAINER
 }
 
-pg-cli() {
+pg_cli() {
   docker run -it --link $DKR_PG_CONTAINER:$DKR_PG_CONTAINER --rm $DKR_IMAGE sh -c 'exec psql -h "$POSTGRES_PORT_5432_TCP_ADDR" -p "$POSTGRES_PORT_5432_TCP_PORT" -U postgres'
 }
 
-alias pg-setup=pg-setup
-alias pg-start=pg-start
-alias pg-stop=pg-stop
-alias pg-restart=pg-restart
-alias pg-cli=pg-cli
+alias pg-setup=pg_setup
+alias pg-start=pg_start
+alias pg-stop=pg_stop
+alias pg-restart=pg_restart
+alias pg-cli=pg_cli
