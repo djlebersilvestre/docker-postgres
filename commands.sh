ed1ea9d60b27ecf55262768e7628fb3db8dbae82
Daniel Silvestre <djlebersilvestre@gmail.com>
2015-03-17 21:39:54 -0300

    Trailling spaces

--- commands.sh
+++ commands.sh
@@ -7,7 +7,9 @@ PG_HOST=127.0.0.1
 PG_PORT=5432
 
 pg_setup() {
-  docker run --name $DKR_PG_CONTAINER -e POSTGRES_PASSWORD=$PG_PASS -d -p $PG_HOST:$PG_PORT:$PG_PORT $DKR_IMAGE
+  if ! docker ps -a | grep -q " $DKR_PG_CONTAINER "; then
+    docker run --name $DKR_PG_CONTAINER -e POSTGRES_PASSWORD=$PG_PASS -d -p $PG_HOST:$PG_PORT:$PG_PORT $DKR_IMAGE
+  fi
 }
 
 pg_start() {
@@ -26,9 +28,3 @@ pg_stop() {
 pg_cli() {
   docker run -it --link $DKR_PG_CONTAINER:$DKR_PG_CONTAINER --rm $DKR_IMAGE sh -c 'exec psql -h "$POSTGRES_PORT_5432_TCP_ADDR" -p "$POSTGRES_PORT_5432_TCP_PORT" -U postgres'
 }
-
-alias pg-setup=pg_setup
-alias pg-start=pg_start
-alias pg-stop=pg_stop
-alias pg-restart=pg_restart
-alias pg-cli=pg_cli
